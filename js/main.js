d1e3f5b605052676f913fb87a77c58622ba4e6d0
@bhattarai.nabraj <bhattarai.nabraj@gmail.com>
2018-09-23 00:52:28 +0545

    new slider added and content added

--- js/main.js
+++ js/main.js
@@ -12,73 +12,8 @@ jQuery(window).load(function(){
 /*  Welcome Section Slider
 /* ========================================================================= */
 
-$(function() {
 
-    var Page = (function() {
-
-        var $navArrows = $( '#nav-arrows' ),
-            $nav = $( '#nav-dots > span' ),
-            slitslider = $( '#slider' ).slitslider( {
-                onBeforeChange : function( slide, pos ) {
-
-                    $nav.removeClass( 'nav-dot-current' );
-                    $nav.eq( pos ).addClass( 'nav-dot-current' );
-
-                }
-            } ),
-
-            init = function() {
-
-                initEvents();
-                
-            },
-            initEvents = function() {
-
-                // add navigation events
-                $navArrows.children( ':last' ).on( 'click', function() {
-
-                    slitslider.next();
-                    return false;
-
-                } );
-
-                $navArrows.children( ':first' ).on( 'click', function() {
-                    
-                    slitslider.previous();
-                    return false;
-
-                } );
-
-                $nav.each( function( i ) {
-                
-                    $( this ).on( 'click', function( event ) {
-                        
-                        var $dot = $( this );
-                        
-                        if( !slitslider.isActive() ) {
-
-                            $nav.removeClass( 'nav-dot-current' );
-                            $dot.addClass( 'nav-dot-current' );
-                        
-                        }
-                        
-                        slitslider.jump( i + 1 );
-                        return false;
-                    
-                    } );
-                    
-                } );
-
-            };
-
-            return { init : init };
-
-    })();
-
-    Page.init();
-
-});
-
+    
 
 
 $(document).ready(function(){
@@ -168,96 +103,26 @@ $(document).ready(function(){
 			}
 		}
 	});
-	
-});
 
+    $('body').on('click', '.moreAbout', function(){
+        $('.more-content').slideDown();
+        $(this).html("Read Less");
+        $(this).addClass('lessAbout');
+        $(this).removeClass('moreAbout');
+    })
+    $('body').on('click', '.lessAbout', function(){
+        $('.more-content').slideUp();
+        $(this).html("Read More");
+        $(this).addClass('moreAbout');
+        $(this).removeClass('lessAbout');
+    })
 
-/* ==========  START GOOGLE MAP ========== */
+    $('#bootstrap-touch-slider').bsTouchSlider();
 
-// When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-
-	    var myLatLng = new google.maps.LatLng(22.402789, 91.822156);
-
-	    var mapOptions = {
-	        zoom: 15,
-	        center: myLatLng,
-	        disableDefaultUI: true,
-	        scrollwheel: false,
-	        navigationControl: true,
-	        mapTypeControl: false,
-	        scaleControl: false,
-	        draggable: true,
-
-        // How you would like to style the map. 
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{
-            featureType: 'water',
-            stylers: [{
-                color: '#46bcec'
-            }, {
-                visibility: 'on'
-            }]
-        }, {
-            featureType: 'landscape',
-            stylers: [{
-                color: '#f2f2f2'
-            }]
-        }, {
-            featureType: 'road',
-            stylers: [{
-                saturation: -100
-            }, {
-                lightness: 45
-            }]
-        }, {
-            featureType: 'road.highway',
-            stylers: [{
-                visibility: 'simplified'
-            }]
-        }, {
-            featureType: 'road.arterial',
-            elementType: 'labels.icon',
-            stylers: [{
-                visibility: 'off'
-            }]
-        }, {
-            featureType: 'administrative',
-            elementType: 'labels.text.fill',
-            stylers: [{
-                color: '#444444'
-            }]
-        }, {
-            featureType: 'transit',
-            stylers: [{
-                visibility: 'off'
-            }]
-        }, {
-            featureType: 'poi',
-            stylers: [{
-                visibility: 'off'
-            }]
-        }]
-    };
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map-canvas');
-
-    // Create the Google Map using our element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Let's also add a marker while we're at it
-    var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(22.402789, 91.822156),
-        map: map,
-		icon: 'img/icons/map-marker.png',
+	('.carousel').carousel({
+      interval: 2000
     });
-}
+});
 
 // ========== END GOOGLE MAP ========== //
 
@@ -266,4 +131,7 @@ var wow = new WOW ({
 	mobile:       false,       // trigger animations on mobile devices (default is true)
 });
 wow.init();
+
+
+
 
